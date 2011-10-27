eec836066f95da655194378a75eb7d949a48ce7b
Dag Odenhall <dag.odenhall@gmail.com>
2011-10-27 20:35:14 +0200

    property test getWord

--- Kibr/Test.hs
+++ Kibr/Test.hs
@@ -16,6 +16,28 @@ runTest args =
     exitWith =<< runTestWithArgs args allHTFTests
 
 
+prop_getWord :: String -> [String] -> Bool -> String -> Maybe String -> Bool
+prop_getWord wordName shapeAffixes shapeExperimental wordDef wordNote =
+    runLA (input >>> getWord) [] == [word]
+  where
+    word  = Word { name = wordName
+                 , shape = Root { affixes = shapeAffixes
+                                , experimental = shapeExperimental
+                                }
+                 , definition = wordDef
+                 , notes = wordNote
+                 }
+    input = mkelem "valsi" [ sattr "word" wordName
+                           , sattr "type" (if shapeExperimental
+                                             then "experimental gismu"
+                                             else "gismu") ]
+                (selem "definition" [txt wordDef] :
+                 [ selem "rafsi" [txt a] | a <- shapeAffixes ] ++
+                 case wordNote of
+                      Just n -> [ selem "notes" [txt n] ]
+                      Nothing -> [])
+
+
 test_getWord :: IO ()
 test_getWord =
   do
