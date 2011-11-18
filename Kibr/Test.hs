5fd871ce7a48aa1b3eba4fd7517350cb3c8773d5
Dag Odenhall <dag.odenhall@gmail.com>
2011-11-18 13:20:30 +0100

    code formatting

--- Kibr/Test.hs
+++ Kibr/Test.hs
@@ -17,43 +17,44 @@ runTest args =
 
 
 prop_getWord :: String -> [String] -> Bool -> String -> Maybe String -> Bool
-prop_getWord wordName shapeAffixes shapeExperimental wordDef wordNote =
+prop_getWord name' affixes' experimental' definition' notes' =
     runLA (input >>> getWord) [] == [word]
   where
-    word  = Word { name = wordName
-                 , shape = Root { affixes = shapeAffixes
-                                , experimental = shapeExperimental
-                                }
-                 , definition = wordDef
-                 , notes = wordNote
+    word  = Word { name       = name'
+                 , shape      = Root { affixes      = affixes'
+                                     , experimental = experimental'
+                                     }
+                 , definition = definition'
+                 , notes      = notes'
                  }
-    input = mkelem "valsi" [ sattr "word" wordName
-                           , sattr "type" (if shapeExperimental
-                                             then "experimental gismu"
-                                             else "gismu") ]
-                (selem "definition" [txt wordDef] :
-                 [ selem "rafsi" [txt a] | a <- shapeAffixes ] ++
-                 case wordNote of
-                      Just n -> [ selem "notes" [txt n] ]
-                      Nothing -> [])
+    input = mkelem "valsi" [ sattr "word" name'
+                           , sattr "type" $ if experimental'
+                                               then "experimental gismu"
+                                               else "gismu"
+                           ]
+                $ selem "definition" [txt definition'] :
+                [ selem "rafsi" [txt a] | a <- affixes' ] ++
+                  case notes' of
+                       Just n  -> [ selem "notes" [txt n] ]
+                       Nothing -> []
 
 
 test_getWord :: IO ()
 test_getWord =
   do
-    [result] <- runX $ readString [] input >>> getWord
-    assertEqual word result
+    result <- runX $ readString [] input >>> getWord
+    assertEqual [word] result
   where
-    word  = Word { name       = "kibro"
-                 , shape      = Root { affixes = [], experimental = True }
-                 , definition = defn
-                 , notes      = Just note
-                 }
-    defn  = "$x_1$ pertains to the internet/cyberspace in aspect $x_2$."
-    note  = "Proposed by xorxes. Short rafsi -kib-. Cf. {mujysamseltcana}."
-    input = concat [ "<valsi unofficial='true' "
-                   , "word='kibro' type='experimental gismu'>"
-                   , "<definition>", defn, "</definition>"
-                   , "<notes>", note, "</notes>"
-                   , "</valsi>"
-                   ]
+    word        = Word { name       = "kibro"
+                       , shape      = Root { affixes = [], experimental = True }
+                       , definition = definition'
+                       , notes      = Just notes'
+                       }
+    definition' = "$x_1$ pertains to the internet/cyberspace in aspect $x_2$."
+    notes'      = "Proposed by xorxes. Short rafsi -kib-. Cf. {mujysamseltcana}."
+    input       = concat [ "<valsi unofficial='true' "
+                         , "word='kibro' type='experimental gismu'>"
+                         , "<definition>", definition', "</definition>"
+                         , "<notes>", notes', "</notes>"
+                         , "</valsi>"
+                         ]
