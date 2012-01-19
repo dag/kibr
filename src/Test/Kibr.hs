678322204ed77266055f6bdaee451518aac162ff
Dag Odenhall <dag.odenhall@gmail.com>
2012-01-19 17:39:01 +0100

    move @?== to Test.HUnit.Diff

--- src/Test/Kibr.hs
+++ src/Test/Kibr.hs
@@ -2,17 +2,14 @@ module Test.Kibr where
 
 import Preamble
 
-import Data.Algorithm.Diff
 import Data.IxSet as Ix
 import Data.Map   as Map
 import Data.Set   as Set
-import Prelude (lines, unlines)
-import System.Console.ANSI
 import System.Environment (withArgs)
 import Test.Framework.Providers.HUnit
 import Test.Framework.TH
 import Test.HUnit hiding (State)
-import Text.Groom
+import Test.HUnit.Diff
 
 import Data.Kibr.Grammar
 import Data.Kibr.Language
@@ -20,19 +17,6 @@ import Data.Kibr.Revision
 import Data.Kibr.State
 import Data.Kibr.Word
 import Text.Kibr.Xml (readDictionary)
-
-(@?==) :: (Eq a, Show a) => a -> a -> Assertion
-x @?== y =
-    assertBool ('\n':msg) (x == y)
-  where
-    msg       = unlines $ fmt <$> getDiff (lines . groom $ x)
-                                          (lines . groom $ y)
-    fmt (B,s) =               ' ' : s
-    fmt (F,s) = color Green $ '+' : s
-    fmt (S,s) = color Red   $ '-' : s
-    color c s = setSGRCode [SetColor Foreground Dull c]
-                ++ s ++
-                setSGRCode [Reset]
 
 run :: [String] -> IO ()
 run args = withArgs args $defaultMainGenerator
