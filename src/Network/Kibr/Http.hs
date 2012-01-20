169b57bd2d6681108c6c6d563606dd77a8069336
Dag Odenhall <dag.odenhall@gmail.com>
2012-01-20 21:52:54 +0100

    clean up http module a bit

--- src/Network/Kibr/Http.hs
+++ src/Network/Kibr/Http.hs
@@ -1,23 +1,23 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
 module Network.Kibr.Http where
  
 import Preamble
+import Prelude (error)
 
 import Data.Acid.Advanced (query')
 import Data.Lens
+import Happstack.Server
 import Language.CSS
 
 import Data.Kibr.Language
 import Data.Kibr.Sitemap
 import Data.Kibr.State
 
-import qualified System.IO            as IO
-
 import qualified Data.Text            as T
 import qualified Data.IxSet           as Ix
-import qualified Happstack.Server     as H
 import qualified System.Log.Logger    as Log
 import qualified Web.Routes           as R
 import qualified Web.Routes.Happstack as R
@@ -25,17 +25,17 @@ import qualified Web.Routes.Happstack as R
 import qualified Text.Kibr.Css        as Css
 import qualified Text.Kibr.Html       as Html
 
-instance H.ToMessage (CSS Rule) where
+instance ToMessage (CSS Rule) where
   toContentType _ = "text/css; charset=UTF-8"
-  toMessage = H.toMessage . renderCSS . runCSS
+  toMessage = toMessage . renderCSS . runCSS
 
-run :: [String] -> Acid -> IO.IO ()
+run :: [String] -> Acid -> IO ()
 run args state =
-  case H.parseConfig args of
-    Left errors  -> mapM_ IO.putStrLn errors
+  case parseConfig args of
+    Left errors  -> error . join $ errors
     Right config -> server config state
 
-server :: H.Conf -> Acid -> IO.IO ()
+server :: Conf -> Acid -> IO ()
 server config state =
   do
     setLogLevel Log.DEBUG
@@ -44,9 +44,9 @@ server config state =
     setLogLevel =
       Log.updateGlobalLogger Log.rootLoggerName . Log.setLevel
     startServer =
-      H.simpleHTTP config $ sum
-        [ H.dir "resources" $ H.serveDirectory H.DisableBrowsing [] "resources"
-        , H.nullDir >> H.seeOther ("/en/"::Text) (H.toResponse (""::Text))
+      simpleHTTP config $ sum
+        [ dir "resources" $ serveDirectory DisableBrowsing [] "resources"
+        , nullDir >> seeOther ("/en/"::Text) (toResponse (""::Text))
         , locale "/en" English
         , locale "/jbo" Lojban
         ]
@@ -55,21 +55,24 @@ server config state =
     locale code =
       R.implSite "" code . site
 
-type Controller = R.RouteT Sitemap (H.ServerPartT IO.IO) H.Response
+type Controller = R.RouteT Sitemap (ServerPartT IO) Response
 
 route :: Acid -> Language -> Sitemap -> Controller
 route st lang url =
   case url of
+    Stylesheet -> stylesheet
     Home       -> home st lang
     Word w     -> word st lang w
-    Stylesheet -> stylesheet
+
+stylesheet :: Controller
+stylesheet = ok . toResponse $ Css.master
 
 home :: Acid -> Language -> Controller
 home st lang =
   do
     db <- query' st ReadState
     page <- Html.master lang . Html.wordList . Ix.toList $ db ^. words
-    H.ok . H.toResponse $ page
+    ok . toResponse $ page
 
 word :: Acid -> Language -> T.Text -> Controller
 word st lang w =
@@ -80,7 +83,4 @@ word st lang w =
     response w'' =
       do
         page <- Html.master lang . Html.wordList $ [w'']
-        H.ok . H.toResponse $ page
-
-stylesheet :: Controller
-stylesheet = H.ok . H.toResponse $ Css.master
+        ok . toResponse $ page
