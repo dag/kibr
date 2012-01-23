631113c589d6313cf86974e926f9d26f6332a8fb
Dag Odenhall <dag.odenhall@gmail.com>
2012-01-23 13:46:38 +0100

    upgrade to ircbot 0.2.1

--- src/Network/Kibr/Irc.hs
+++ src/Network/Kibr/Irc.hs
@@ -2,30 +2,36 @@ module Network.Kibr.Irc where
 
 import Preamble
 
-import Control.Concurrent (killThread)
-import Data.Acid.Advanced (query')
+import Control.Concurrent            (killThread)
+import Data.Acid.Advanced            (query')
 import Data.Kibr.State
-import Data.Text          (pack)
+import Data.Text                     (pack)
 import Network.IRC.Bot
-import Network.IRC.Bot.Part.Ping
-import System.IO          (getLine)
+import Network.IRC.Bot.Part.Channels (initChannelsPart)
+import Network.IRC.Bot.Part.NickUser (nickUserPart)
+import Network.IRC.Bot.Part.Ping     (pingPart)
+import System.IO                     (getLine)
 import Text.Parsec
+
+import qualified Data.Set as Set
 
 run :: [String] -> Acid -> IO ()
 run _ state =
   do
-    threads <- simpleBot conf [pingPart, wordPart state]
+    (_, channelsPart) <- initChannelsPart . Set.fromList $ ["#sampla"]
+    threads <- simpleBot conf [ pingPart
+                              , nickUserPart
+                              , channelsPart
+                              , wordPart state
+                              ]
     getLine
     mapM_ killThread threads
   where
     conf = nullBotConf { host = "irc.freenode.net"
                        , nick = "kibr"
-                       , user = User { username = "kibr"
-                                     , hostname = "."
-                                     , servername = "."
-                                     , realname = "kibr bot"
-                                     }
-                       , channel = "#sampla"
+                       , user = nullUser { username = "kibr"
+                                         , realname = "kibr bot"
+                                         }
                        }
 
 wordPart :: BotMonad m => Acid -> m ()
