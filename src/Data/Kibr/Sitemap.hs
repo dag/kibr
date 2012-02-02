5c1ab55a4f7b30eb175eb9d783537edf6a540fb8
Dag Odenhall <dag.odenhall@gmail.com>
2012-02-02 18:34:05 +0100

    don't need all those instances for sitemaps

--- src/Data/Kibr/Sitemap.hs
+++ src/Data/Kibr/Sitemap.hs
@@ -7,13 +7,11 @@ import Web.Routes.TH (derivePathInfo)
 data Asset
   = Highlighter
   | Screen
-  deriving (Eq, Ord, Read, Show, Data, Typeable)
 
 derivePathInfo ''Asset
 
 data Sitemap
   = Home
   | Word Text
-  deriving (Eq, Ord, Read, Show, Data, Typeable)
 
 derivePathInfo ''Sitemap
