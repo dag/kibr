13760dfa349edc67b3eeb1c9e1e10680afa23dcc
Dag Odenhall <dag.odenhall@gmail.com>
2012-07-26 10:47:57 +0200

    move commands to CLI module

--- src/Kibr/CLI.hs
+++ src/Kibr/CLI.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE CPP, FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, OverloadedStrings #-}
+{-# LANGUAGE CPP, FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, OverloadedStrings, QuasiQuotes, RecordWildCards #-}
 
 -- | Command-line interface support code.
 module Kibr.CLI
@@ -25,35 +25,56 @@ module Kibr.CLI
     , runProgramT
       -- * Utilities
     , io
+      -- * Commands
+    , program
+    , runServe
+    , runImport
+    , runCheckpoint
+    , runLookup
+    , runSearch
     )
   where
 
+import Prelude   hiding (catch)
 import Kibr.Data hiding (User)
 
 import qualified Data.HashMap.Lazy as HashMap
 import qualified Data.Set          as Set
 import qualified Happstack.Server  as Happstack
 
+import Control.Concurrent             (forkIO, killThread)
+import Control.Exception              (bracket, catch, throw)
+import Control.Monad                  (forM_, when, unless)
 import Control.Monad.Reader           (MonadReader, ReaderT, runReaderT, asks)
 import Control.Monad.Trans            (MonadIO, liftIO)
-import Data.Acid                      (AcidState)
+import Data.Acid                      (AcidState, createCheckpoint)
+import Data.Acid.Remote               (acidServer)
 import Data.Conf                      (Conf(conf))
 import Data.Has                       (Has(fetch))
 import Data.Maybe                     (fromMaybe)
 import Data.String                    (fromString)
 import Data.Text                      (Text)
+import Happstack.Server.SimpleHTTP    (waitForTermination)
 import Kibr.State
+import Kibr.Text                      (ppWord, ppWords)
+import Kibr.XML                       (readDictionary)
 import Network                        (HostName, PortID(..))
 import Network.IRC.Bot                (BotConf(..), User(..))
 import Options.Applicative
+import System.Directory               (removeFile)
 import System.Environment             (getEnvironment)
 import System.Environment.XDG.BaseDir (getUserDataDir, getUserCacheDir)
 import System.FilePath                ((</>))
 import System.IO                      (stdout, hIsTerminalDevice)
+import System.IO.Error                (isDoesNotExistError)
+import Text.InterpolatedString.Perl6  (qq)
 import Text.PrettyPrint.ANSI.Leijen   (Doc, (<>), displayIO, renderPretty, plain, linebreak)
+import Text.XML.HXT.Core              ((/>), runX, readDocument, withTrace)
+import Text.XML.HXT.HTTP              (withHTTP)
 
 #ifndef WINDOWS
 import System.Console.Terminfo        (getCapability, termColumns, setupTermFromEnv)
+import Text.XML.HXT.Expat             (withExpat)
 #endif
 
 
@@ -232,3 +253,65 @@ instance Monad m => Has OutputMode (ProgramT m) where
 -- | Convenient alias for 'liftIO'.
 io :: MonadIO m => IO a -> m a
 io = liftIO
+
+
+-- * Commands
+-- ***************************************************************************
+
+-- | Dispatches the commands.
+program :: Program
+program = do
+    cmd <- asks (cmd . options)
+    case cmd of
+      Serve services        -> runServe services
+      Import traceLevel doc -> runImport traceLevel doc
+      Checkpoint            -> runCheckpoint
+      Lookup words          -> runLookup words
+      Search keywords       -> runSearch keywords
+
+runServe :: [Service] -> Program
+runServe services = do
+    state <- asks state
+    Config{..} <- asks config
+    when (State `elem` services) $ io $
+      do (_,port) <- stateServer
+         bracket (forkIO $ acidServer state port)
+                 (\tid ->
+                   do killThread tid
+                      case port of
+                        UnixSocket path -> tryRemoveFile path
+                        _ -> return ())
+                 (const waitForTermination)
+  where
+    tryRemoveFile fp  = removeFile fp `catch` ignoreNotExists
+    ignoreNotExists e = unless (isDoesNotExistError e) $ throw e
+
+runImport :: TraceLevel -> FilePath -> Program
+runImport traceLevel doc = do
+    dict <- io $ runX $ readDocument sys doc /> readDictionary
+    output "Importing..."
+    update_ $ ImportWords dict
+    let total = sum $ map (length . snd) dict
+    output [qq|Finished importing $total words.|]
+  where
+    sys = [withHTTP [], withTrace traceLevel]
+#ifndef WINDOWS
+            ++ [withExpat True]
+#endif
+
+runCheckpoint :: Program
+runCheckpoint = io . createCheckpoint =<< asks state
+
+runLookup :: [Word] -> Program
+runLookup words = do
+    language <- asks (language . options)
+    forM_ words $ \word ->
+      do Just typ <- query $ LookupWordType word
+         Just def <- query $ LookupWordDefinition word language
+         output $ linebreak <> ppWord word typ def
+
+runSearch :: [Text] -> Program
+runSearch keywords = do
+    language <- asks (language . options)
+    words <- query $ SearchKeyWords (map (KeyWord language . DefinitionWord) keywords)
+    output $ ppWords words
--- src/Kibr/Run.hs
+++ src/Kibr/Run.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE CPP, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, OverloadedStrings, QuasiQuotes, RecordWildCards #-}
+{-# LANGUAGE RecordWildCards #-}
 
 -- | Support for configuration via dynamic recompilation using
 -- "Config.Dyre".
@@ -18,47 +18,17 @@ module Kibr.Run
     ( kibr
     , CompilationError
     , main
-    , program
-    , runServe
-    , runImport
-    , runCheckpoint
-    , runLookup
-    , runSearch
     )
   where
 
-import Prelude hiding ((.), catch)
-
-import Config.Dyre                   (Params(..), wrapMain, defaultParams)
-import Control.Category              ((.))
-import Control.Concurrent            (forkIO, killThread)
-import Control.Exception             (bracket, catch, throw)
-import Control.Monad                 (forM_, when, unless)
-import Control.Monad.Reader          (asks)
-import Data.Acid                     (AcidState, openLocalStateFrom, createCheckpoint)
-import Data.Acid.Remote              (acidServer, openRemoteState)
-import Data.Default                  (def)
-import Data.Text                     (Text)
-import Happstack.Server.SimpleHTTP   (waitForTermination)
+import Config.Dyre         (Params(..), wrapMain, defaultParams)
+import Data.Acid           (openLocalStateFrom)
+import Data.Acid.Remote    (openRemoteState)
+import Data.Default        (def)
 import Kibr.CLI
-import Kibr.Data
-import Kibr.State
-import Kibr.Text                     (ppWord, ppWords)
-import Kibr.XML                      (readDictionary)
-import Network                       (PortID(UnixSocket))
-import Options.Applicative           ((<*>), execParser, info, helper, fullDesc)
-import System.Directory              (removeFile)
-import System.Exit                   (exitFailure)
-import System.IO                     (hPutStr, stderr)
-import System.IO.Error               (isDoesNotExistError)
-import Text.InterpolatedString.Perl6 (qq)
-import Text.PrettyPrint.ANSI.Leijen  ((<>), linebreak)
-import Text.XML.HXT.Core             ((/>), runX, readDocument, withTrace)
-import Text.XML.HXT.HTTP             (withHTTP)
-
-#ifndef WINDOWS
-import Text.XML.HXT.Expat            (withExpat)
-#endif
+import Options.Applicative ((<*>), execParser, info, helper, fullDesc)
+import System.Exit         (exitFailure)
+import System.IO           (hPutStr, stderr)
 
 -- | The @kibr@ executable.
 kibr :: Config -> IO ()
@@ -86,61 +56,3 @@ main (Right config@Config{..}) = do
                              openRemoteState host port
     openAcidState False = do dir <- stateDirectory
                              openLocalStateFrom dir def
-
--- | Dispatches the commands.
-program :: Program
-program = do
-    cmd <- asks (cmd . options)
-    case cmd of
-      Serve services        -> runServe services
-      Import traceLevel doc -> runImport traceLevel doc
-      Checkpoint            -> runCheckpoint
-      Lookup words          -> runLookup words
-      Search keywords       -> runSearch keywords
-
-runServe :: [Service] -> Program
-runServe services = do
-    state <- asks state
-    Config{..} <- asks config
-    when (State `elem` services) $ io $
-      do (_,port) <- stateServer
-         bracket (forkIO $ acidServer state port)
-                 (\tid ->
-                   do killThread tid
-                      case port of
-                        UnixSocket path -> tryRemoveFile path
-                        _ -> return ())
-                 (const waitForTermination)
-  where
-    tryRemoveFile fp  = removeFile fp `catch` ignoreNotExists
-    ignoreNotExists e = unless (isDoesNotExistError e) $ throw e
-
-runImport :: TraceLevel -> FilePath -> Program
-runImport traceLevel doc = do
-    dict <- io $ runX $ readDocument sys doc /> readDictionary
-    output "Importing..."
-    update_ $ ImportWords dict
-    let total = sum $ map (length . snd) dict
-    output [qq|Finished importing $total words.|]
-  where
-    sys = [withHTTP [], withTrace traceLevel]
-#ifndef WINDOWS
-            ++ [withExpat True]
-#endif
-
-runCheckpoint :: Program
-runCheckpoint = io . createCheckpoint =<< asks state
-
-runLookup :: [Word] -> Program
-runLookup words = do
-    language <- asks (language . options)
-    forM_ words $ \word ->
-      do Just typ <- query $ LookupWordType word
-         Just def <- query $ LookupWordDefinition word language
-         output $ linebreak <> ppWord word typ def
-
-runSearch :: [Text] -> Program
-runSearch keywords = do
-    language <- asks (language . options)
-    words <- query $ SearchKeyWords (map (KeyWord language . DefinitionWord) keywords)
-    output $ ppWords words
