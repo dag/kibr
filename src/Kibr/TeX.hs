24cd2f53845de49580d2307f18d7ed1d32a8d3b6
Dag Odenhall <dag.odenhall@gmail.com>
2012-08-05 16:43:36 +0200

    tex parser

---
+++ src/Kibr/TeX.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Kibr.TeX
+    ( -- * Types
+      TeX
+    , Inline(..)
+    , Expr(..)
+      -- * Parsers
+    , tex
+    , str
+    , emph
+    , bold
+    , expr
+    , lit
+    , sub
+    , sup
+    )
+  where
+
+import Control.Applicative
+import Data.Char
+import Data.Text (Text)
+import Data.Attoparsec.Text as P
+
+type TeX = [Inline]
+
+data Inline = Str Text   -- ^ A sequence of text.
+            | Emph Text  -- ^ Emphasized text.
+            | Bold Text  -- ^ Bold text.
+            | Expr Expr  -- ^ A mathematical expression.
+            deriving (Eq, Show)
+
+data Expr = Lit Text       -- ^ A literal number or variable.
+          | Sub Expr Expr  -- ^ Subscript.
+          | Sup Expr Expr  -- ^ Superscript.
+          deriving (Eq, Show)
+
+tex :: Parser TeX
+tex = many (emph <|> bold <|> expr <|> str) <?> "tex"
+
+str :: Parser Inline
+str = Str <$> P.takeWhile1 (\chr -> chr /= '\\' && chr /= '$') <?> "str"
+
+emph :: Parser Inline
+emph = Emph <$> ("\\emph{" .*> P.takeWhile (/= '}') <* P.take 1) <?> "emph"
+
+bold :: Parser Inline
+bold = Bold <$> ("\\textbf{" .*> P.takeWhile (/= '}') <* P.take 1) <?> "bold"
+
+expr :: Parser Inline
+expr = Expr <$> (char '$' *> (sub <|> sup) <* char '$') <?> "expr"
+
+lit :: Parser Expr
+lit =
+    Lit <$> (open *> alnum <* close <|> alnum) <?> "lit"
+  where
+    alnum = takeWhile1 isAlphaNum
+    open  = char '{'
+    close = char '}'
+
+sub :: Parser Expr
+sub = Sub <$> lit <*> (char '_' *> lit) <?> "sub"
+
+sup :: Parser Expr
+sup = Sup <$> lit <*> (char '^' *> lit) <?> "sup"
